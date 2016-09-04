03ff54b1de3d4b574925e0197e84ac79073a62c6
Ian Roberts <ianroberts131@gmail.com>
2016-09-04 07:25:38 -0700

    Checkpoint 29: completed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,25 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var animatePoints = function(points) {
+var animatePoints = function() {
     
-    var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "rotate(360deg)";
-        points[index].style.msTransform = "rotate(360deg)";
-        points[index].style.WebkitTransform = "rotate(360deg)";
-    };
-    
-    for (var i = 0; i < points.length; i++) {
-        revealPoint(i);
-    };
+    var revealPoint = function() {
+        $(this).css({
+					opacity: 1,
+					transform: 'scaleX(1) translateY(0)'
+				});
+		};
+	
+		$.each($('.point'), revealPoint);
     
 };
 
-window.onload = function() {
-    if (window.innerHeight > 950) {
-        animatePoints(pointsArray);
-    }
+$(window).load(function() {
+    if ($(window).height() > 950) {
+			animatePoints();
+		}
     
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-            animatePoints(pointsArray);
-        }
-    });
-}
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    $(window).scroll(function(event) {
+			if ($(window).scrollTop() >= scrollDistance) {
+				animatePoints();
+			}
+		});
+});
