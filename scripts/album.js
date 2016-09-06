5a239897f981af3723680dec5c9f8d32121366d8
Ian Roberts <ianroberts131@gmail.com>
2016-09-05 21:05:53 -0700

    Assignment 33: completed

--- scripts/album.js
+++ scripts/album.js
@@ -147,6 +147,18 @@ var previousSong = function() {
 	$nextPlayingCell.html(pauseButtonTemplate);
 };
 
+var togglePlayFromPlayerBar = function() {
+	if (currentSoundFile.isPaused()) {
+		getSongNumberCell(currentlyPlayingSongNumber).html(pauseButtonTemplate);
+		$('.main-controls .play-pause').html(playerBarPauseButton);
+		currentSoundFile.play();
+	} else {
+		getSongNumberCell(currentlyPlayingSongNumber).html(playButtonTemplate);
+		$('.main-controls .play-pause').html(playerBarPlayButton);
+		currentSoundFile.pause();
+	}
+};
+
 var updatePlayerBarSong = function() {
 	$('.currently-playing .song-name').text(currentSongFromAlbum.title);
 	$('.currently-playing .artist-name').text(currentAlbum.artist);
@@ -165,11 +177,13 @@ var currentSoundFile = null;
 var currentVolume = 80;
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+var $playButton = $('.main-controls .play-pause');
 
 $(document).ready(function() {
 	setCurrentAlbum(albumPicasso);
 	$previousButton.click(previousSong);
 	$nextButton.click(nextSong);
+	$playButton.click(togglePlayFromPlayerBar);
 });
 
 
